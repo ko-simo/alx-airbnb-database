498301d5400818368b535f4e97221082f91e6740
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:41:56 +0100

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,7 +1,7 @@
-CREATE INDEX idx_bookings_user_id ON bookings(user_id);
+CREATE INDEX idx_booking_user_id ON Booking(user_id);
 
-CREATE INDEX idx_bookings_property_id ON bookings(property_id);
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
 
-CREATE INDEX idx_reviews_property_id ON reviews(property_id);
+CREATE INDEX idx_review_property_id ON Review(property_id);
 
-CREATE INDEX idx_bookings_start_date ON bookings(start_date);
+CREATE INDEX idx_booking_start_date ON Booking(start_date);
