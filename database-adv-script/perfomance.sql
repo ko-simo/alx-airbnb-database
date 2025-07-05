f4b3dc9b4b51192107c3261c310454aee2d2dbba
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:10:13 +0100

    Create perfomance.sql

---
+++ database-adv-script/perfomance.sql
@@ -0,0 +1,26 @@
+SELECT
+    b.id AS booking_id,
+    b.start_date,
+    b.end_date,
+    u.name AS user_name,
+    p.name AS property_name,
+    pay.amount AS payment_amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON pay.booking_id = b.id
+WHERE b.start_date >= '2024-01-01'
+ORDER BY b.start_date DESC;
+
+SELECT
+    b.id,
+    b.start_date,
+    u.name,
+    p.name,
+    pay.amount
+FROM bookings b
+JOIN users u ON b.user_id = u.id
+JOIN properties p ON b.property_id = p.id
+JOIN payments pay ON pay.booking_id = b.id
+WHERE b.start_date >= '2024-01-01'
+ORDER BY b.start_date DESC;
