44f35bc22afa284fb70c0ea879c0c0e7b2fd23ed
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:52:20 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,16 +1,16 @@
+-- Fetch bookings with user, property, and payment details
 SELECT
     b.booking_id,
     b.start_date,
     b.end_date,
-    u.user_id,
     u.name,
-    p.property_id,
     p.property_name,
-    pay.payment_id,
-    pay.amount,
-    pay.payment_date
+    pay.amount
 FROM
     Booking b
-INNER JOIN User u ON b.user_id = u.user_id
-INNER JOIN Property p ON b.property_id = p.property_id
-LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
+JOIN
+    User u ON b.user_id = u.user_id
+JOIN
+    Property p ON b.property_id = p.property_id
+LEFT JOIN
+    Payment pay ON b.booking_id = pay.booking_id;
