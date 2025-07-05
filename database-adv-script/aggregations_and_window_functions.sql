cd74c3a0d026be0799e00ae15d5c7a6196f3e797
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:39:46 +0100

    Update aggregations_and_window_functions.sql

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,21 +1,17 @@
-
-SELECT
-    user_id,
-    COUNT(*) AS total_bookings
-FROM bookings
-GROUP BY user_id;
-
-WITH property_booking_counts AS (
-    SELECT
-        property_id,
-        COUNT(*) AS total_bookings
-    FROM bookings
-    GROUP BY property_id
-)
-
 SELECT
     property_id,
+    property_name,
     total_bookings,
-    RANK() OVER (ORDER BY total_bookings DESC) AS booking_rank
-FROM property_booking_counts;
-
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank
+FROM (
+    SELECT
+        p.property_id,
+        p.property_name,
+        COUNT(b.booking_id) AS total_bookings
+    FROM
+        Property p
+    LEFT JOIN
+        Booking b ON p.property_id = b.property_id
+    GROUP BY
+        p.property_id, p.property_name
+) AS subquery;
