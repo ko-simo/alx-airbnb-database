8c3a0cc67851f22323027e12332e1e0996589951
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:25:21 +0100

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,36 +1,39 @@
 SELECT
-    bookings.id AS booking_id,
-    bookings.start_date,
-    bookings.end_date,
-    users.id AS user_id,
-    users.name,
-    users.email
+  bookings.id,
+  bookings.start_date,
+  bookings.end_date,
+  users.id,
+  users.name,
+  users.email
 FROM bookings
 INNER JOIN users ON bookings.user_id = users.id;
 
 SELECT
-    properties.id AS property_id,
-    properties.name,
-    reviews.id AS review_id,
-    reviews.rating,
-    reviews.comment
+  properties.id,
+  properties.name,
+  reviews.id,
+  reviews.rating,
+  reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
 
 SELECT
-    users.id AS user_id,
-    users.name,
-    bookings.id AS booking_id,
-    bookings.start_date
+  users.id,
+  users.name,
+  bookings.id,
+  bookings.start_date
 FROM users
 LEFT JOIN bookings ON users.id = bookings.user_id
 
 UNION
 
 SELECT
-    users.id AS user_id,
-    users.name,
-    bookings.id AS booking_id,
-    bookings.start_date
+  users.id,
+  users.name,
+  bookings.id,
+  bookings.start_date
 FROM users
-RIGHT JOIN bookings ON users.id = bookings.user_id;
+RIGHT JOIN bookings ON users.id = bookings.user_id
+WHERE users.id IS NULL
+ORDER BY id;
