b98c6faab81d1e427a1dfeda700b16cec66f672c
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-05 16:46:07 +0100

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,26 +1,16 @@
 SELECT
-    b.id AS booking_id,
+    b.booking_id,
     b.start_date,
     b.end_date,
-    u.name AS user_name,
-    p.name AS property_name,
-    pay.amount AS payment_amount
-FROM bookings b
-JOIN users u ON b.user_id = u.id
-JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON pay.booking_id = b.id
-WHERE b.start_date >= '2024-01-01'
-ORDER BY b.start_date DESC;
-
-SELECT
-    b.id,
-    b.start_date,
+    u.user_id,
     u.name,
-    p.name,
-    pay.amount
-FROM bookings b
-JOIN users u ON b.user_id = u.id
-JOIN properties p ON b.property_id = p.id
-JOIN payments pay ON pay.booking_id = b.id
-WHERE b.start_date >= '2024-01-01'
-ORDER BY b.start_date DESC;
+    p.property_id,
+    p.property_name,
+    pay.payment_id,
+    pay.amount,
+    pay.payment_date
+FROM
+    Booking b
+INNER JOIN User u ON b.user_id = u.user_id
+INNER JOIN Property p ON b.property_id = p.property_id
+LEFT JOIN Payment pay ON b.booking_id = pay.booking_id;
